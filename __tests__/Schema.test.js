8feeb0cc3228b62666c0f5d62c955df0a2396e62
sanvean74 <dlambertpdx@gmail.com>
2019-09-19 09:54:15 -0700

    tests finally passing

--- __tests__/Schema.test.js
+++ __tests__/Schema.test.js
@@ -1,41 +1,48 @@
 const Schema = require('../lib/Schema');
+const { ModelError } = require('../lib/Errors');
 
 describe('Schema', () => {
   const personSchema = {
-    firstName: {
-      type: 'string',
-    },
-    lastName: {
-      type: 'string',
-    },
-    color: {
-      type: 'string',
-    },
-    married: {
-      type: 'boolean',
-    },
-    kids: {
-      type: 'number'
-    }
-
+    firstName: { type: 'string', },
+    lastName: { type: 'string', },
+    color: { type: 'string', },
+    married: { type: 'boolean', },
+    kids: { type: 'number' }
   };
-
 
   const schemaValidator = new Schema(personSchema);
   // add a test schema
   const sampleData = {
     'firstName': 'Chris',
     'lastName': 'Sample',
+    'color': 'Red',
+    'married': true,
+    'kids': 3
+  };
+
+  const castingSampleData = {
+    'firstName': 'Chris',
+    'lastName': 'Sample',
+    'color': 'Red',
+    'married': 'true',
+    'kids': '3'
+  };
+
+  const invalidSampleData = {
+    'firstName': [],
+    'lastName': 'Sample',
+    'color': 'Red',
     'married': true,
     'kids': 3
   };
 
   it('validates a correct model', () => {
-
-  });
-
-  it('throws on invalid model', () => {
     expect(schemaValidator.validate(sampleData)).toEqual(sampleData);
+    expect(schemaValidator.validate(castingSampleData)).toEqual(sampleData);
+  });
+  
+  it('throws on invalid model', () => {
+    expect(() => schemaValidator.validate(invalidSampleData)).toThrow(ModelError);
   });
 
   // more test cases...
